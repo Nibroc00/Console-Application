66592c51cdd8457cd6951827b2e1eb29f00f079e
Nibroc Parke <nibroc04@gmail.com>
2021-03-20 12:13:14 -0600

    Creation and Completetion

---
+++ 3x3/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+
+namespace Console_Application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool z = true; 
+            char[,] grid = new char[5, 5] {
+                {'_', '_', '_', '_', '_'}, 
+                {'|', '.', ' ', ' ', '|'}, 
+                {'|', ' ', ' ', ' ', '|'}, 
+                {'|', ' ', ' ', ' ', '|'}, 
+                {'_', '_', '_', '_', '_'}
+            };
+            int[] position = {1, 1};
+            for(int i = 0; i < 5; i++) {
+                for(int j = 0; j < 5; j++) {
+                    Console.Write(grid[i, j]);
+                }
+                Console.Write('\n');
+            }
+            while(z) {
+                if (Console.KeyAvailable) {
+                    var input = Console.ReadKey(true);
+                    switch (input.Key) {
+                        case ConsoleKey.S:
+                            if (position[0] < 3) {
+                                grid[position[0], position[1]] = ' ';
+                                grid[position[0] + 1, position[1]] = '.';
+                                position[0]++;
+                            }
+                            break;
+                        case ConsoleKey.W:
+                            if (position[0] > 1) {
+                                grid[position[0], position[1]] = ' ';
+                                grid[position[0] - 1, position[1]] = '.';
+                                position[0]--;
+                            }
+                            break;
+                        case ConsoleKey.D:
+                            if (position[1] < 3) {
+                                grid[position[0], position[1]] = ' ';
+                                grid[position[0], position[1] + 1] = '.';
+                                position[1]++;
+                            }
+                            break;
+                        case ConsoleKey.A:
+                            if (position[1] > 1) {
+                                grid[position[0], position[1]] = ' ';
+                                grid[position[0], position[1] - 1] = '.';
+                                position[1]--;
+                            }
+                            break;
+                        case ConsoleKey.DownArrow:
+                            if (position[0] < 3) {
+                                grid[position[0], position[1]] = ' ';
+                                grid[position[0] + 1, position[1]] = '.';
+                                position[0]++;
+                            }
+                            break;
+                        case ConsoleKey.UpArrow:
+                            if (position[0] > 1) {
+                                grid[position[0], position[1]] = ' ';
+                                grid[position[0] - 1, position[1]] = '.';
+                                position[0]--;
+                            }
+                            break;
+                        case ConsoleKey.RightArrow:
+                            if (position[1] < 3) {
+                                grid[position[0], position[1]] = ' ';
+                                grid[position[0], position[1] + 1] = '.';
+                                position[1]++;
+                            }
+                            break;
+                        case ConsoleKey.LeftArrow:
+                            if (position[1] > 1) {
+                                grid[position[0], position[1]] = ' ';
+                                grid[position[0], position[1] - 1] = '.';
+                                position[1]--;
+                            }
+                            break;
+                        case ConsoleKey.Q:
+                            z = false;
+                            break;
+                        default:
+                            break;
+
+                    }
+                    Console.Clear();
+                    for(int i = 0; i < 5; i++) {
+                    for(int j = 0; j < 5; j++) {
+                        Console.Write(grid[i, j]);
+                    }
+                    Console.Write('\n');
+            }
+                }
+            }
+        }
+    }
+}
---
+++ Single Line/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Console_Application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool z = true; 
+            char[] line = {'|', '.', ' ', ' ', ' ', ' ', '|'};
+            int position = 1;
+            Console.WriteLine(line);
+            while(z) {
+                if (Console.KeyAvailable) {
+                    var input = Console.ReadKey(true);
+                    // char x = Convert.ToChar(input);
+                                // Console.Clear();
+                                // Console.WriteLine("r");
+                    switch (input.Key) {
+                        case ConsoleKey.D:
+                            if (position < 5) {
+                                line[position] = ' ';
+                                line[position + 1] = '.';
+                                position++;
+                            }
+                            break;
+                        case ConsoleKey.A:
+                            if (position > 1) {
+                                line[position] = ' ';
+                                line[position - 1] = '.';
+                                position--;
+                            }
+                            break;
+                        case ConsoleKey.RightArrow:
+                            if (position < 5) {
+                                line[position] = ' ';
+                                line[position + 1] = '.';
+                                position++;
+                            }
+                            break;
+                        case ConsoleKey.LeftArrow:
+                            if (position > 1) {
+                                line[position] = ' ';
+                                line[position - 1] = '.';
+                                position--;
+                            }
+                            break;
+                        case ConsoleKey.Q:
+                            z = false;
+                            break;
+                        default:
+                            break;
+
+                    }
+                    Console.Clear();
+                    Console.WriteLine(line);
+                }
+            }
+        }
+    }
+}
